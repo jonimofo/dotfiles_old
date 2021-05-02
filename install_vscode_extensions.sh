c0e3762fe8e9120a3de5c5a7a12bcd11a542a309
mofo <mofo@mofo.com>
2021-05-02 22:37:13 +0200

    Add VScode Extension install script

---
+++ install_vscode_extensions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+code --install-extension aaron-bond.better-comments
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension cstrap.python-snippets
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
+code --install-extension njpwerner.autodocstring
