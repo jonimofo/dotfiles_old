b0fae55c3767802224ebe5c896a3c9a98de48511
bgi <bgi@qdv.fr>
2021-06-30 14:16:33 +0200

    Update vscode extensions file

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -3,14 +3,22 @@
 extensions=(
 aaron-bond.better-comments
 azemoh.one-monokai
+buenon.scratchpads
 CoenraadS.bracket-pair-colorizer
 cstrap.python-snippets
 DavidAnson.vscode-markdownlint
 dbaeumer.vscode-eslint
 esbenp.prettier-vscode
+Gruntfuggly.align-mode
+mads-hartmann.bash-ide-vscode
 ms-python.python
+ms-python.vscode-pylance
 ms-toolsai.jupyter
 njpwerner.autodocstring
+piotrpalarz.vscode-gitignore-generator
+sourcery.sourcery
+timonwong.shellcheck
+yandeu.five-server
 )
 
 
